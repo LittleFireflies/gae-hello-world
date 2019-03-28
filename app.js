9aed4cba428a100ad4467210f0937cbc89628ccb
Widyarso Joko Purnomo <widyjp10@gmail.com>
2019-03-28 13:43:14 +0700

    update text message

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-    res.send('Hello, world!')
+    res.send('Hello, world! Welcome to Google App Engine!')
 })
 
 const PORT = process.env.PORT || 8000
